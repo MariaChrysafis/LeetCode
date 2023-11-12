7ca527ea17f27fda5a2337bfb8bc63c4a28c7486
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2023-11-12 12:04:38 -0500

    Update 2932.go

--- 2932.go
+++ 2932.go
@@ -8,7 +8,7 @@ func maximumStrongPairXor(nums []int) int {
     ans := 0
     for x := 0; x < len(nums); x++ {
         for y := 0; y < len(nums); y++ {
-            if abs(nums[x] - nums[y]) < min(nums[x], nums[y]) {
+            if abs(nums[x] - nums[y]) <= min(nums[x], nums[y]) {
                 ans = max(ans, (nums[x] ^ nums[y]))
             }
         }
